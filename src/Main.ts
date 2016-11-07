b7633e5ddf3bd9b3affdf775b30d7f0c625f005b
wjt19960403 <3098548617@qq.com>
2016-11-07 10:10:25 +0800

    Finish Homework 6

--- src/Main.ts
+++ src/Main.ts
@@ -14,6 +14,7 @@
 //       names of its contributors may be used to endorse or promote products
 //       derived from this software without specific prior written permission.
 //
+//
 //  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
 //  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 //  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
